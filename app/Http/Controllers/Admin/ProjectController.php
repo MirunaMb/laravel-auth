c631fa0f5ee75f66cc57e2df33d82d17d7d32cef
MirunaMb <mirunamarcela98@gmail.com>
2023-10-24 21:04:03 +0200

    add routes

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -105,7 +105,8 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-        //
+      $projects = Project::findOrfail($project->id);
+      return view('admin.projects.edit', compact('projects'));
     }
 
     /**
@@ -117,7 +118,47 @@ class ProjectController extends Controller
      */
     public function update(Request $request, Project $project)
     {
-        //
+        $data = $request->all();
+
+        $validator = Validator::make(
+            $data,
+            [
+                'title' => 'required|string|max:75',
+                'content'=>'required|string|max:75',
+                'slug' => 'required|string|max:75',
+                'created' => 'required|string|max:75',
+                'updated' => 'required|string|max:75',
+            ],
+            [
+                'title.required' => 'Il titolo è obbligatorio',
+                'title.string' => 'Il titolo deve essere una stringa',
+                'title.max' => 'Il titolo deve contenere meno di 75 caratteri',
+                'content.required' => 'Il content è obbligatorio',
+                'content.string' => 'Il content deve essere una stringa',
+                'content.max' => 'Il content deve contenere meno di 75 caratteri',
+
+                'slug.required' => 'Lo slug è obbligatorio',
+                'slug.string' => 'Lo slug deve essere una stringa',
+                'slug.max' => 'Lo slug deve contenere meno di 75 caratteri',
+                'created.required' => 'Il campo "creato" è obbligatorio',
+                'created.string' => 'Il campo "creato" deve essere una stringa',
+                'created.max' => 'Il campo "creato" deve contenere meno di 75 caratteri',
+                'updated.required' => 'Il campo "aggiornato" è obbligatorio',
+                'updated.string' => 'Il campo "aggiornato" deve essere una stringa',
+                'updated.max' => 'Il campo "aggiornato" deve contenere meno di 75 caratteri',
+            ],
+        );
+        if ($validator->fails()) {
+            return redirect()
+                ->route('admin.projects.edit', $project->id) // Se la validazione fallisce, reindirizza all'azione di modifica
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $project = Project::findOrFail($project->id);
+        $project->update($data);
+
+        return redirect()->route('admin.projects.show', $project->id);
     }
 
     /**
@@ -128,6 +169,8 @@ class ProjectController extends Controller
      */
     public function destroy(Project $project)
     {
-        //
+        $projects = Project::findOrFail($project->id);
+        $projects->delete();
+        return redirect()->route('admin.projects.index');
     }
 }
